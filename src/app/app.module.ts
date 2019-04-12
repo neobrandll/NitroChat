530c735558f83be9946b89c9bdd0b3d354a33b43
Luis A. Acurero <13luismb@gmail.com>
2019-04-11 20:58:30 -0400

    minor changes on dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SocketIoModule, SocketIoConfig, Socket} from 'ng-socket-io';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
  
 const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
 
